225823e6a2b8f7193e78864403ffd7f3ca329f7b
tejasthakare45 <tejasthakare4518@gmail.com>
2026-02-17 21:45:41 +0530

    updated run.bat to use python -m pytest

--- Run.bat
+++ Run.bat
@@ -1,2 +1,2 @@
-pytest -v -n=2 --html=Html_Reports\OrangeHRM_Login_chrome.html --alluredir=Allure_Reports --browser chrome
-pytest -v -n=2 --html=Html_Reports\OrangeHRM_Login_firefox.html --alluredir=Allure_Reports --browser firefox
+python -m pytest -v -n=2 --html=Html_Reports\OrangeHRM_Login_chrome.html --alluredir=Allure_Reports --browser chrome
+python -m pytest -v -n=2 --html=Html_Reports\OrangeHRM_Login_firefox.html --alluredir=Allure_Reports --browser firefox
