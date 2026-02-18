0a7bf1c36d0b1f7d919fe815a4961e85aed9ef39
tejasthakare45 <tejasthakare4518@gmail.com>
2026-02-18 11:19:08 +0530

    added 2 scripts

--- Run.bat
+++ Run.bat
@@ -1,1 +1,2 @@
-
+"C:\Users\HP ProBook\AppData\Local\Programs\Python\Python314\python.exe" -m pytest -v -n=2 --html=Html_Reports\OrangeHRM_Login_chrome.html --alluredir=Allure_Reports --browser chrome
+"C:\Users\HP ProBook\AppData\Local\Programs\Python\Python314\python.exe" -m pytest -v -n=2 --html=Html_Reports\OrangeHRM_Login_firefox.html --alluredir=Allure_Reports --browser firefox
