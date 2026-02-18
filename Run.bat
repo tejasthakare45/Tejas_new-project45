8901df386cc0a0b00a4e018f97704df08ec47685
tejasthakare45 <tejasthakare4518@gmail.com>
2026-02-18 14:00:04 +0530

    updated run.bat scripts

--- Run.bat
+++ Run.bat
@@ -1,1 +1,3 @@
+"C:\Users\HP ProBook\AppData\Local\Programs\Python\Python311\python.exe" -m pytest -v -n=2 --html=Html_Reports\OrangeHRM_Login_chrome.html --alluredir=Allure_Reports --browser chrome
 
+"C:\Users\HP ProBook\AppData\Local\Programs\Python\Python311\python.exe" -m pytest -v -n=2 --html=Html_Reports\OrangeHRM_Login_firefox.html --alluredir=Allure_Reports --browser firefox
